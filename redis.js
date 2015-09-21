30bef93d8814b2124f9f857b72930ba30922e733
Jonathan Hunter <jonathan.hunter@laterooms.com>
2015-09-21 09:00:54 +0100

    closing redis connection after each call as there seems to be a bug if the connection is held open for long periods of time

--- redis.js
+++ redis.js
@@ -6,44 +6,57 @@ var Promise = require("bluebird");
 var redisPort = config.get('redis:port');
 var redisHost = config.get('redis:host')
 
-var client = redis.createClient(redisPort, redisHost);
-
 module.exports = function() {
 
     var getHash = function(hash){
+
+        var client = redis.createClient(redisPort, redisHost);
+
         return new Promise(function(resolve, reject){
             client.hgetall(hash, function (err, obj) {
 
                 if(err) {
+                    client.quit();
                     reject(err);
                 }
 
+                client.quit();
                 resolve(obj);
             });
         })
     }
 
     var setHash = function(hash, obj){
+
+        var client = redis.createClient(redisPort, redisHost);
+
         return new Promise(function(resolve, reject){
             client.hmset(hash, obj, function (err, obj) {
 
                 if(err) {
+                    client.quit();
                     reject(err);
                 }
 
+                client.quit();
                 resolve(obj);
             });
         })
     }
 
     var deleteHash = function(hash){
+
+        var client = redis.createClient(redisPort, redisHost);
+
         return new Promise(function(resolve, reject){
             client.del(hash, function (err, obj) {
 
                 if(err) {
+                    client.quit();
                     reject(err);
                 }
 
+                client.quit();
                 resolve(obj);
             });
         })        
