e9c511d5e3fed90733b8e6a7ef07368a0399700d
Steve Elliott <steve@tegud.net>
2015-10-16 21:42:00 +0100

    Refactored out overlay logic

--- victoropsoncall.js
+++ victoropsoncall.js
@@ -8,6 +8,8 @@ var moment = require('moment');
 var isApiSecure = config.get('victorOpsApi:isSecure') ? 'https://' : 'http://';
 var hostname = config.get('victorOpsApi:hostname');
 
+var getOnCallRotationForAllTeams = require('./lib/schedule');
+
 module.exports = function(){
 
     var authorisationString = config.get('victorOpsCredentials:username') + ':' + config.get('victorOpsCredentials:password');
@@ -82,139 +84,6 @@ module.exports = function(){
       return _.first(matchingOverlays);
     };
 
-    var getOnCallRotationForAllTeams = function(organisationOnCallData) {
-      return new Promise(function(resolve, reject){
-
-        if(organisationOnCallData === '' || organisationOnCallData === null){
-          reject('no oncall data was passed');
-        }
-
-        var onCall = _.reduce(organisationOnCallData, function(allTeams, team) {
-          var currentRotation = _.chain(team.oncall).filter(function(rotation) { return rotation.oncall }).first().value();
-          var allRotations =  _.sortBy(Array.prototype.concat.apply([], _.pluck(team.oncall, 'rolls')), function(rotation) {
-            return rotation.change;
-          });
-
-          if(!currentRotation) {
-            allTeams[team.name] = {
-              current: 'No-one',
-              schedule: []
-            };
-
-            return allTeams;
-          }
-
-          function shiftContainedByOverride(rotationStart, rotationEnd, overlayStart, overlayEnd) {
-            return (overlayStart.isBefore(rotationStart) || overlayStart.isSame(rotationStart)) 
-              && (overlayEnd.isSame(rotationEnd) || overlayEnd.isAfter(rotationEnd));
-          }
-
-          allTeams[team.name] = {
-            current: currentRotation.overrideoncall || currentRotation.oncall,
-            schedule: _.reduce(allRotations, function(allRotations, rotation, i) {
-              var rotationStart = moment(rotation.change);
-              var rotationEnd = moment(rotation.until);
-              var oncall = rotation.oncall;
-
-              var matchingOverlay = _.chain(team.overlays).filter(function(overlay) {
-                var overlayStart = moment(overlay.start);
-                var overlayEnd = moment(overlay.end);
-
-                return oncall === overlay.orig && (((overlayStart.isAfter(rotationStart) || overlayStart.isSame(rotationStart)) 
-                  && (overlayEnd.isBefore(rotationEnd) || overlayEnd.isSame(rotationEnd)))
-                  || shiftContainedByOverride(rotationStart, rotationEnd, overlayStart, overlayEnd));
-              }).first().value();
-
-              var newRotations = [];
-
-              if(matchingOverlay) {
-                matchingOverlay = JSON.parse(JSON.stringify(matchingOverlay));
-
-                if(moment(matchingOverlay.start).isBefore(rotationStart)) {
-                  matchingOverlay.start = rotationStart.valueOf();
-                }
-
-                if(moment(matchingOverlay.end).isAfter(rotationEnd)) {
-                  matchingOverlay.end = rotationEnd.valueOf();
-                }
-
-                var newRotations = [];
-
-                if(moment(matchingOverlay.start).isSame(rotationStart) && moment(matchingOverlay.end).isSame(rotationEnd)) {
-                  newRotations.push({
-                    oncall: matchingOverlay.over,
-                    start: moment(matchingOverlay.start).utc().format(),
-                    end: moment(matchingOverlay.end).utc().format()
-                  });
-                }
-                else if(moment(matchingOverlay.start).isSame(rotationStart)) {
-                  newRotations = newRotations.concat([{
-                      oncall: matchingOverlay.over,
-                      start: moment(matchingOverlay.start).utc().format(),
-                      end: moment(matchingOverlay.end).utc().format()
-                    },
-                    {
-                      oncall: oncall,
-                      start: moment(matchingOverlay.end).utc().format(),
-                      end: rotationEnd.utc().format()
-                    }]);
-                }
-                else if(moment(matchingOverlay.end).isSame(rotationEnd)) {
-                  newRotations = newRotations.concat([{
-                      oncall: oncall,
-                      start: rotationStart.utc().format(),
-                      end: moment(matchingOverlay.start).utc().format()
-                    },
-                    {
-                      oncall: matchingOverlay.over,
-                      start: moment(matchingOverlay.start).utc().format(),
-                      end: moment(matchingOverlay.end).utc().format()
-                    }]);
-                }
-                else {
-                  newRotations = newRotations.concat([{
-                      oncall: oncall,
-                      start: rotationStart.utc().format(),
-                      end: moment(matchingOverlay.start).utc().format()
-                    },
-                    {
-                      oncall: matchingOverlay.over,
-                      start: moment(matchingOverlay.start).utc().format(),
-                      end: moment(matchingOverlay.end).utc().format()
-                    },
-                    {
-                      oncall: oncall,
-                      start: moment(matchingOverlay.end).utc().format(),
-                      end: rotationEnd.utc().format()
-                    }]);
-                }
-              }
-              else {
-                newRotations.push({
-                  oncall: oncall,
-                  start: rotationStart.utc().format(),
-                  end: rotationEnd.utc().format()
-                });
-              }
-
-              if(newRotations.length && allRotations.length && allRotations[allRotations.length - 1].oncall === newRotations[0].oncall) {
-                allRotations[allRotations.length - 1].end = newRotations.shift().end;
-              }
-
-              return allRotations.concat(newRotations);
-            }, [])
-          };
-
-
-          return allTeams;
-        }, {});
-
-        resolve({
-          teams: onCall
-        });
-      });
-    }
-
     var getPeopleOnCallForAllTeams = function(organisationOnCallData) {
 
       return new Promise(function(resolve, reject){
@@ -260,12 +129,12 @@ module.exports = function(){
     }
 
   return {
-    getOnCallRotaForAllTeams : getOnCallRotaForAllTeams,
-    getOnCallRotaForTeam : getOnCallRotaForTeam,
-    getPersonOnCallForTeam : getPersonOnCallForTeam,
-    getPeopleOnCallForAllTeams : getPeopleOnCallForAllTeams,
+    getOnCallRotaForAllTeams: getOnCallRotaForAllTeams,
+    getOnCallRotaForTeam: getOnCallRotaForTeam,
+    getPersonOnCallForTeam: getPersonOnCallForTeam,
+    getPeopleOnCallForAllTeams: getPeopleOnCallForAllTeams,
     getOnCallRotationForAllTeams: getOnCallRotationForAllTeams,
-    hasRotaChanged : hasRotaChanged
+    hasRotaChanged: hasRotaChanged
   };
 
 }();
